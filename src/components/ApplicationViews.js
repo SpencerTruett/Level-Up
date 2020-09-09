a4267d4b6ee29aa4fe78611dea0e28fcb38fa60d
Spencer Truett <srtruett1993@gmail.com>
2020-09-09 09:27:19 -0500

    added initial files

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+export const ApplicationViews = (props) => {
+  return (
+    <>
+
+      <Route exact path="/">
+        {/* <List/> */}
+      </Route>
+
+      <Route path="/">
+        {/* <List/> */}
+      </Route>
+
+    </>
+  )
+}
