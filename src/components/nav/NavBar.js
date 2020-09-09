281896ee961a6bc373e70331ca092a56a27e96f8
Spencer Truett <srtruett1993@gmail.com>
2020-09-09 09:36:55 -0500

    adjusted Nav slightly

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -4,10 +4,20 @@ import "./NavBar.css"
 
 export const NavBar = (props) => {
   return (
-    <ul className="navbar">
-      <li className="navbar__item">
-        <Link className="navbar__link" to="/logout">Logout</Link>
-      </li>
-    </ul>
+    <>
+      <ul className="navbar">
+        <li className="navbar__item">
+          <Link className="navbar__link" to="/logout">Logout</Link>
+        </li>
+      </ul>
+      <ul>
+        <li className="navbar">
+          <Link className="navbar__link" to="/events">Events</Link>
+        </li>
+        <li className="navbar">
+          <Link className="navbar__link" to="/chat">Chat</Link>
+        </li>
+      </ul>
+    </>
   )
 }
